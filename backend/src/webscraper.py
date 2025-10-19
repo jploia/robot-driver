eb478ef5d8a116c151476667914197fba0c9c560
Julia Nguyen <juliatnguyen05@gmail.com>
2025-10-18 18:32:18 -0700

    feat: add basic webpage launch

--- backend/src/webscraper.py
+++ backend/src/webscraper.py
@@ -1,1 +1,12 @@
 # this will include the code for the webscraper
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    # p.firefox.launch(headless=False, slow_mo=50)
+    browser = p.firefox.launch()
+    page = browser.new_page()
+    page.goto("https://playwright.dev")
+    print(page.title())
+    browser.close()
+
+
