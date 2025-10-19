564ea197e27233d83c5dc2492c407f703b32f6b6
Julia Nguyen <juliatnguyen05@gmail.com>
2025-10-18 20:34:59 -0700

    fix: bypass continue shopping button

--- backend/src/webscraper.py
+++ backend/src/webscraper.py
@@ -1,20 +1,50 @@
 # this will include the code for the webscraper
 from playwright.sync_api import sync_playwright, expect
 
-def open_page(url: str) -> None:
+# primary function containing the functionality of the program
+def search_item(url: str, query: str) -> str:
+    '''Returns a string of the title of the first result from the given 
+    query and url'''
+    # open page -- remove function to stay within the event loop?
+    # browser, page = _open_page(url)
+    with sync_playwright() as p:
+        browser = p.firefox.launch(headless=False, slow_mo=50) # headless for testing purposes
+        page = browser.new_page()
+        try:
+            page.goto(url)
+        except:
+            raise Exception(f'Sorry, the page associated with {url} does not exist.')
+
+        print(f'The page, {page.title()}, has successfully been opened.')
+    
+        # bypass continue shopping button
+        page.get_by_role("button").click()
+
+        # conduct search
+        # search bar ID: id="nav-search-bar-form"
+        page.get_by_role("nav-search-bar-form").dispatch_event('click')
+        
+        print('yay')
+
+        # close browser
+        browser.close()
+
+
+def _open_page(url: str) -> '(playwright.Browser, playwright.page)':
     '''Opens the provided url and prints a success message if successful,
-    otherwise nothing is printed'''
+    otherwise nothing is printed. Returns the browser.'''
     with sync_playwright() as p:
         browser = p.firefox.launch()
         page = browser.new_page()
         try:
             page.goto(url)
         except:
-            raise Exception(f'Sorry, the page associated with {url} may not exist.')
+            raise Exception(f'Sorry, the page associated with {url} does not exist.')
 
         print(f'The page, {page.title()}, has successfully been opened.')
-        browser.close()
+        return browser, page
 
 # for testing purposes only
-# if __name__ == '__main__':
-#     open_page('https://amazon.com/')
+if __name__ == '__main__':
+    # _open_page('https://amazon.com/')
+    search_item('https://amazon.com', 'bananas')
