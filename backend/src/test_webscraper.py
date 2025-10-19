da3b53eaa0a85bce67c3d89358b1477cba8ea671
Julia Nguyen <juliatnguyen05@gmail.com>
2025-10-18 19:32:50 -0700

    feat: add function to pass in url to open

--- backend/src/test_webscraper.py
+++ backend/src/test_webscraper.py
@@ -1,4 +1,19 @@
-import unittest
+import unittest, webscraper, io, sys
+
+URL = 'https://www.amazon.com/'
+
+class TestWebscrapperMethods(unittest.TestCase):
+
+    def test_valid_url_can_open_page(self):
+        output = io.StringIO()
+        sys.stdout = output # redirect stdout
+        webscraper.open_page(URL)    
+        sys.stdout = sys.__stdout__ # reset redirect
+        output = output.getvalue()
+        self.assertEqual(output, 'The page, Amazon.com, has successfully been opened.\n')
+
+        
+
 
 if __name__ == '__main__':
     unittest.main()
--- backend/src/webscraper.py
+++ backend/src/webscraper.py
@@ -1,12 +1,19 @@
 # this will include the code for the webscraper
 from playwright.sync_api import sync_playwright
 
-with sync_playwright() as p:
-    # p.firefox.launch(headless=False, slow_mo=50)
-    browser = p.firefox.launch()
-    page = browser.new_page()
-    page.goto("https://playwright.dev")
-    print(page.title())
-    browser.close()
+def open_page(url: str) -> None:
+    '''Opens the provided url and prints a success message if successful,
+    otherwise nothing is printed'''
+    with sync_playwright() as p:
+        browser = p.firefox.launch()
+        page = browser.new_page()
+        page.goto(url)
 
+        # error check if the page has been opened
 
+        print(f'The page, {page.title()}, has successfully been opened.')
+        browser.close()
+
+# for testing purposes only
+# if __name__ == '__main__':
+#     open_page('https://amazon.com/')
